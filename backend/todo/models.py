9dd55fa9e13bee54630c66e3cff94b68c8c4b09f
Irene <irenehhuynh2@gmail.com>
2024-08-26 11:07:31 -0400

    Tailwind setup

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Todo(models.Model):
+    body = models.CharField(max_length=300)
+    completed = models.BooleanField(default=False)
+    # Each time you create a new Todo, it auto creates the date created
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.body
--- backend/todo/views.py
+++ backend/todo/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from . import models
+from . import serializers
 
 # Create your views here.
+
+class TodoViewSet(viewsets.ModelViewSet):
+    queryset = models.Todo.objects.all()
+    serializer_class= serializers.TodoSerializer
+    
